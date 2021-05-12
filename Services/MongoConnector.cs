8cf7be4229ce999a80b6d6736ca0b6547cce4346
Pratik <pratik1070061@gmail.com>
2021-05-11 21:35:15 -0400

    testing policy issue

--- Services/MongoConnector.cs
+++ Services/MongoConnector.cs
@@ -93,7 +93,7 @@ namespace policy_issue.Services
             var result = collection.Find(builders).ToList();
 
             //RemoveIdObject(result);
-            Console.WriteLine("Getting data from mongo - " + collectionName + " - " + objectName + " - " + new JArray { result.Select(x => JObject.Parse(x.ToJson())).ToArray() });
+            Console.WriteLine("Getting data from mongo - " + collectionName + " - " + objectName + " - " + quoteId + " - " + new JArray { result.Select(x => JObject.Parse(x.ToJson())).ToArray() });
             return new JArray { result.Select(x => JObject.Parse(x.ToJson())).ToArray() };
             //return new JObject(new JProperty(objectName,new JArray { result.Select(x=> new JValue(x.ToJson())).ToArray() } ));
 
